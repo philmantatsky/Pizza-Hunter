a79a7f90ff2f47404a6e1924fe6ff06460b1990c
Phillip Mantatsky <pmantatsky1@bsd220.org>
2024-02-28 09:34:42 -0600

    Initial Commit

---
+++ Pizza Hunter/Pizza_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_HunterApp.swift
+//  Pizza Hunter
+//
+//  Created by Phillip Mantatsky on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
